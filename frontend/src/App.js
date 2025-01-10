a9af731562a4647ccb4160c3a1d0d5505245815b
Shieng <137869411+Shiengg@users.noreply.github.com>
2025-01-10 10:35:37 +0700

    Push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
 import DeliveryDashboard from './pages/DeliveryDashboard';
@@ -15,7 +15,6 @@ import VehicleManagement from './components/Admin/VehicleManagement';
 import NotFound from './components/Shared/NotFound';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AdminContext } from './contexts/AdminContext';
 import { Toaster } from 'react-hot-toast';
 import { NotificationProvider } from './contexts/NotificationContext';
 import CustomerMap from './pages/CustomerMap';
@@ -39,87 +38,86 @@ function App() {
           },
         }}
       />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          
-          {/* Public Routes */}
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          
-          {/* Admin Dashboard Routes */}
-          <Route 
-            path="/admin-dashboard/*" 
-            element={
-              <ProtectedRoute allowedRoles={['Admin']}>
-                <AdminDashboard />
-              </ProtectedRoute>
-            } 
-          >
-            <Route path="shops" element={<ShopManagement />} />
-            <Route path="routes" element={<RouteManagement />} />
-            <Route path="vehicle-types" element={<VehicleManagement />} />
-          </Route>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        
+        {/* Public Routes */}
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        
+        {/* Admin Dashboard Routes */}
+        <Route 
+          path="/admin-dashboard/*" 
+          element={
+            <ProtectedRoute allowedRoles={['Admin']}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          } 
+        >
+          <Route path="shops" element={<ShopManagement />} />
+          <Route path="routes" element={<RouteManagement />} />
+          <Route path="vehicle-types" element={<VehicleManagement />} />
+        </Route>
 
-          <Route 
-            path="/delivery-dashboard" 
-            element={
-              <ProtectedRoute allowedRoles={['DeliveryStaff']}>
-                <DeliveryDashboard />
-              </ProtectedRoute>
-            } 
-          />
+        <Route 
+          path="/delivery-dashboard" 
+          element={
+            <ProtectedRoute allowedRoles={['DeliveryStaff']}>
+              <DeliveryDashboard />
+            </ProtectedRoute>
+          } 
+        />
 
-          <Route 
-            path="/delivery/map/:id" 
-            element={
-              <ProtectedRoute allowedRoles={['DeliveryStaff']}>
-                <DeliveryMap />
-              </ProtectedRoute>
-            } 
-          />
-          
-          <Route 
-            path="/customer-tracking" 
-            element={
-              <ProtectedRoute allowedRoles={['Customer']}>
-                <CustomerTracking />
-              </ProtectedRoute>
-            } 
-          />
+        <Route 
+          path="/delivery/map/:id" 
+          element={
+            <ProtectedRoute allowedRoles={['DeliveryStaff']}>
+              <DeliveryMap />
+            </ProtectedRoute>
+          } 
+        />
+        
+        <Route 
+          path="/customer-tracking" 
+          element={
+            <ProtectedRoute allowedRoles={['Customer']}>
+              <CustomerTracking />
+            </ProtectedRoute>
+          } 
+        />
 
-          {/* Profile & Settings Routes - accessible by all authenticated users */}
-          <Route 
-            path="/profile" 
-            element={
-              <ProtectedRoute allowedRoles={['Admin', 'DeliveryStaff', 'Customer']}>
-                <Profile />
-              </ProtectedRoute>
-            } 
-          />
-          
-          <Route 
-            path="/settings" 
-            element={
-              <ProtectedRoute allowedRoles={['Admin', 'DeliveryStaff', 'Customer']}>
-                <Settings />
-              </ProtectedRoute>
-            } 
-          />
+        {/* Profile & Settings Routes - accessible by all authenticated users */}
+        <Route 
+          path="/profile" 
+          element={
+            <ProtectedRoute allowedRoles={['Admin', 'DeliveryStaff', 'Customer']}>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
+        
+        <Route 
+          path="/settings" 
+          element={
+            <ProtectedRoute allowedRoles={['Admin', 'DeliveryStaff', 'Customer']}>
+              <Settings />
+            </ProtectedRoute>
+          } 
+        />
 
-          <Route 
-            path="/tracking/map/:routeCode" 
-            element={
-              <ProtectedRoute allowedRoles={['Customer']}>
-                <CustomerMap />
-              </ProtectedRoute>
-            } 
-          />
+        <Route 
+          path="/tracking/map/:routeCode" 
+          element={
+            <ProtectedRoute allowedRoles={['Customer']}>
+              <CustomerMap />
+            </ProtectedRoute>
+          } 
+        />
 
-          <Route path="/" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer />
     </NotificationProvider>
   );
 }
